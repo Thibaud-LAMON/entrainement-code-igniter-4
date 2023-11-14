6852978519971ef4309ab083e81cbb60cec7f7f1
Thibaud-LAMON <thib.lamon@gmail.com>
2023-11-14 11:30:59 +0100

    How To Use Query Builder Methods

--- first-ci4/app/Config/Routes.php
+++ first-ci4/app/Config/Routes.php
@@ -8,3 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index'); //route principale, toutes les routes suivent le modèle : $routes->[requête http]('/[URI]', '[contrôleur]::[méthode]);
 $routes->get('/about-us', 'Sites::index');
 $routes->get('/services', 'ServiceController::index');
+$routes->get('add-student', 'Sites::insertStudent');
+$routes->get('update-student', 'Sites::updateStudent');
+$routes->get('delete-student', 'Sites::deleteStudent');
+$routes->get('students', 'Sites::selectStudents');
--- first-ci4/app/Controllers/Sites.php
+++ first-ci4/app/Controllers/Sites.php
@@ -6,8 +6,66 @@ use App\Controllers\BaseController;
 
 class Sites extends BaseController
 {
-    public function index()
+    private $db;
+    private $table;
+    public function __construct()//création des objets table et BDD via un constructeur
     {
-        //
+        $this->db = db_connect();  //on se connecte à la BDD définie dans .env
+        $this->table = $this->db->table("students"); //on agit sur la table student
+    }
+    public function insertStudent()
+    {
+        $data = [
+            "name" => "Jacques Beauregard",
+            "email" => "jb@gmail.com",
+            "phone" => "0611223300",
+        ];
+
+        if($this->table->insert($data)){
+            echo "<h3>Student created!</h3>";
+        }else{
+            echo "<h3>Failed to create student!</h3>";
+        }
+        
+    }
+    public function updateStudent()
+    {
+        $student_id = 2;
+
+        $updated_data= [ //données mises à jour
+            "email" => "jb@outlook.net",
+        ];
+
+        if($this->table->update($updated_data, [ //on doit indiquer l'id de l'entrée concernée
+            "id" => $student_id
+        ])){
+            echo "<h3>Student updated!</h3>";
+        }else{
+            echo "<h3>Failed to update student!</h3>";
+        }
+    }
+    public function deleteStudent()
+    {
+        $student_id = 2;
+
+        if($this->table->delete([
+            "id" => $student_id
+        ])){
+            echo "<h3>Student deleted!</h3>";
+        }else{
+            echo "<h3>Failed to delete student!</h3>";
+        }
+    }
+    public function selectStudents()
+    {
+
+        $students = $this->table->where([
+            "id" => 3
+        ])->get()->getRowArray();
+        
+        echo "<pre>";
+
+        print_r($students);
+        
     }
 }
